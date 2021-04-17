d5feafffff0634c4ef179fd982105ec4d6a5531f
Tkaulfers15 <78368562+Tkaulfers15@users.noreply.github.com>
2021-04-17 14:12:42 -0400

    Edited Final Screen
    -Created Game objects to be displayed at the end, showing the questions asked
    -set the correct answers' color to green
    -When a button is pressed and is not correct. the choice will come up red on the Final Screen

--- Assets/LowFidelityTrainer/Scripts/ManagerScripts/FinalScreenManager.cs
+++ Assets/LowFidelityTrainer/Scripts/ManagerScripts/FinalScreenManager.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class FinalScreenManager : MonoBehaviour
 {
     public GameObject WrongQuestion;
     public Canvas FinalCanvas;
     public int x;
+    public int z;
     public GameObject QuestText;
     public GameObject AnswerCheckText;
     public int yVector;
+    public GameObject Test;
+
+    public GameObject AnswerAText;
+    public GameObject AnswerBText;
+    public GameObject AnswerCText;
+    public GameObject AnswerDText;
+
+    public GameObject Managers;
+    private UIManager UI;
+
+    public GameObject[] OptionsA;
+    public GameObject[] OptionsB;
+    public GameObject[] OptionsC;
+    public GameObject[] OptionsD;
+    
     // Start is called before the first frame update
     void Start()
     {
+        z = 0;
+        UI = Managers.GetComponent<UIManager>();
         yVector = 600;
-        Debug.Log(GameObject.FindGameObjectWithTag("ResultsCanvas"));
+       // Debug.Log(GameObject.FindGameObjectWithTag("ResultsCanvas"));
         //GameObject x12 =  GameObject.Instantiate(WrongQuestion, new Vector3(0, 0, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("ResultsCanvas").transform);
 
         //  GameObject y = GameObject.FindGameObjectWithTag("ResultsCanvas");
         Debug.Log(QuestText.GetComponentInParent<TextMeshProUGUI>().text);
+        
 
     }
 
@@ -39,7 +59,7 @@ public class FinalScreenManager : MonoBehaviour
         {
             for (int i = 0; i < 1; i++)
             {
-                Debug.Log(QuestText.GetComponentInParent<TextMeshProUGUI>().text);
+               // Debug.Log(QuestText.GetComponentInParent<TextMeshProUGUI>().text);
                 WrongQuestion.GetComponentInChildren<TextMeshProUGUI>().text = QuestText.GetComponentInParent<TextMeshProUGUI>().text;
                 GameObject.Instantiate(WrongQuestion, new Vector3(850, yVector, 0), Quaternion.identity, FinalCanvas.transform);
 
@@ -52,5 +72,112 @@ public class FinalScreenManager : MonoBehaviour
 
 
 
-    
+
+    public void FinalCheckA()
+    {
+        Debug.Log(OptionsA[0].GetComponentInChildren<TextMeshProUGUI>().text);
+        // Debug.Log(AnswerCText.GetComponentInParent<TextMeshProUGUI>().text);
+        // Debug.Log(UI.AnswersArrayC[1]);
+
+        for (int i = 0; i < UI.AnswersArrayA.Length; i++)
+        {
+            if (AnswerAText.GetComponentInParent<TextMeshProUGUI>().text != UI.AnswersArrayA[i])
+            {
+                z += 1;
+            }
+            if (z == UI.AnswersArrayA.Length)
+            {
+                for (int j = 0; j < OptionsA.Length; j++)
+                {
+                    if (OptionsA[j].GetComponentInChildren<TextMeshProUGUI>().text == AnswerAText.GetComponentInParent<TextMeshProUGUI>().text)
+                    {
+                        OptionsA[j].GetComponentInChildren<TextMeshProUGUI>().color = Color.red;
+                    }
+                }
+            }
+        }
+        z = 0;
+    }
+
+
+
+    public void FinalCheckB()
+    {
+        //Debug.Log(OptionsA[0].GetComponentInChildren<TextMeshProUGUI>().text);
+        // Debug.Log(AnswerCText.GetComponentInParent<TextMeshProUGUI>().text);
+        // Debug.Log(UI.AnswersArrayC[1]);
+
+        for (int i = 0; i < UI.AnswersArrayB.Length; i++)
+        {
+            if (AnswerBText.GetComponentInParent<TextMeshProUGUI>().text != UI.AnswersArrayB[i])
+            {
+                z += 1;
+            }
+            if (z == UI.AnswersArrayB.Length)
+            {
+                for (int j = 0; j < OptionsB.Length; j++)
+                {
+                    if (OptionsB[j].GetComponentInChildren<TextMeshProUGUI>().text == AnswerBText.GetComponentInParent<TextMeshProUGUI>().text)
+                    {
+                        OptionsB[j].GetComponentInChildren<TextMeshProUGUI>().color = Color.red;
+                    }
+                }
+            }
+        }
+        z = 0;
+    }
+    public void FinalCheckC()
+    {
+        Debug.Log(OptionsC[0].GetComponentInChildren<TextMeshProUGUI>().text);
+       // Debug.Log(AnswerCText.GetComponentInParent<TextMeshProUGUI>().text);
+       // Debug.Log(UI.AnswersArrayC[1]);
+        
+        for (int i = 0; i < 6; i++)
+        {
+            if (AnswerCText.GetComponentInParent<TextMeshProUGUI>().text != UI.AnswersArrayC[i])
+            {
+                z += 1;
+            }
+            if (z == UI.AnswersArrayC.Length)
+            {
+                for (int j = 0; j < OptionsC.Length; j++)
+                {
+                    if (OptionsC[j].GetComponentInChildren<TextMeshProUGUI>().text == AnswerCText.GetComponentInParent<TextMeshProUGUI>().text)
+                    {
+                        OptionsC[j].GetComponentInChildren<TextMeshProUGUI>().color = Color.red;
+                    }
+                }
+            }
+        }
+        z = 0;
+    }
+
+
+
+    public void FinalCheckD()
+    {
+        //Debug.Log(OptionsD[0].GetComponentInChildren<TextMeshProUGUI>().text);
+        // Debug.Log(AnswerCText.GetComponentInParent<TextMeshProUGUI>().text);
+        // Debug.Log(UI.AnswersArrayC[1]);
+
+        for (int i = 0; i < UI.AnswersArrayD.Length; i++)
+        {
+            if (AnswerDText.GetComponentInParent<TextMeshProUGUI>().text != UI.AnswersArrayD[i])
+            {
+                z += 1;
+            }
+            if (z == UI.AnswersArrayD.Length)
+            {
+                for (int j = 0; j < OptionsD.Length; j++)
+                {
+                    if (OptionsD[j].GetComponentInChildren<TextMeshProUGUI>().text == AnswerDText.GetComponentInParent<TextMeshProUGUI>().text)
+                    {
+                        OptionsD[j].GetComponentInChildren<TextMeshProUGUI>().color = Color.red;
+                    }
+                }
+            }
+        }
+        z = 0;
+    }
+
 }
